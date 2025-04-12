6208acf39a241c0a0bb39a16efb52d9d872c6397
Lance <enaganlance@gmail.com>
2025-04-12 14:30:28 +0800

    added quiz empty list, then questions will be add there

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,5 +1,8 @@
 # Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. Write the collected data to a text file. Ask another question until the user chose to exit.
 
+# Empty quiz list
+quiz = []
+
 # Get the user's questions until he/she chose to exit
 while True:
     print("\n=== NEW QUESTION ===")
@@ -22,5 +25,12 @@ while True:
         correct = input("Which is correct? (A/B/C/D): ")
         if correct not in ['a', 'b', 'c', 'd', 'A', 'B', 'C', 'D']:
             print("Please enter A, B, C, or D.")
+
+    # Adding questions to the quiz list
+    quiz.append({
+        'question': question,
+        'a': choice_a, 'b': choice_b, 'c': choice_c, 'd': choice_d,
+        'correct': correct.lower()
+    })
 
 # Save the questions to a text file
