bafc9909f4305c8f6cb54399faddaf4c2db6c479
Lance <enaganlance@gmail.com>
2025-05-06 19:30:30 +0800

    pseudocode

---
+++ quiz_program.py
@@ -0,0 +1,11 @@
+# Create the Quiz program that read the output file of the Quiz Creator. The user will answer the randomly selected question and check if the answer is correct.
+
+# User's input of the text file name to read the file
+# Extracting the contents of the text file
+    # Extract the questions
+    # Extract the choices
+    # Extract the answers
+# Displaying the questions and choices to the user
+# Get the user's answers
+# Check if correct
+# Display the results of the quiz
