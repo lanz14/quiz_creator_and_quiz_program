4c747fd14276710060881c4a5a67d5965bc38e16
Lance <enaganlance@gmail.com>
2025-05-08 22:17:42 +0800

    check if the file exists

--- quiz_program.py
+++ quiz_program.py
@@ -1,5 +1,7 @@
 # Create the Quiz program that read the output file of the Quiz Creator. The user will answer the randomly selected question and check if the answer is correct.
 
+import os
+
 # User's input of the text file name to read the file
 filename = input("\nEnter the quiz file name: ")
 if not filename.endswith('.txt'):
@@ -9,6 +11,13 @@ if not filename.endswith('.txt'):
     # Extract the questions
     # Extract the choices
     # Extract the answers
+questions = []
+
+if not os.path.exists(filename):
+    print(f"Error: File '{filename}' not found.")
+else:
+    pass
+
 # Displaying the questions and choices to the user
 # Get the user's answers
 # Check if correct
