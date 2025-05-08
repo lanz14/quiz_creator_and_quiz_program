8a3e183fc98ca5f729f96ecf9864a104df9bcc9a
Lance <enaganlance@gmail.com>
2025-05-08 22:04:59 +0800

    ask the user for the file name of the quiz

--- quiz_program.py
+++ quiz_program.py
@@ -1,6 +1,10 @@
 # Create the Quiz program that read the output file of the Quiz Creator. The user will answer the randomly selected question and check if the answer is correct.
 
 # User's input of the text file name to read the file
+filename = input("\nEnter the quiz file name: ")
+if not filename.endswith('.txt'):
+    filename += '.txt'
+
 # Extracting the contents of the text file
     # Extract the questions
     # Extract the choices
